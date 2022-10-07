d90d3c30f8d17453ad7926e89990ea17bf19359d
Renee Doll <renee@reneedoll.com>
2022-10-07 10:33:01 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const addFunction = (addition1, addition2) => addition1 + addition2;
+const addingNum = addFunction(4, 3)
+console.log(addingNum)
+
+const multiFunction = (multiply) => multiply * 2;
+const multiNum = multiFunction(8)
+console.log(multiNum)
